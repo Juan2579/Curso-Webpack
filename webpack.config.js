ed0343bde92bdfb0dc614fed107b854600d9202c
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-11-18 17:49:18 -0500

    Added loaders for css, images and fonts, added minimization and alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "[name].[contenthash].js"
   },
   resolve: {
-    extensions: [".js"]
+    extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    }
   },
   module: {
     rules: [
@@ -25,6 +34,17 @@ module.exports = {
       use: [MiniCssExtractPlugin.loader,
       "css-loader"
       ],
+    },
+    {
+      test: /\.png/,
+      type: "asset/resource"
+    },
+    {
+      test: /\.(woff|woff2)$/i,
+      type: "asset/resource",
+      generator: {
+        filename: "assets/font/[hash][ext][query]"
+      }
     }
     ]
   },
@@ -34,6 +54,23 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html"
     }),
-    new MiniCssExtractPlugin(),
-  ]
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
